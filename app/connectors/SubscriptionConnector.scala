76f1c073d243f2cc14d763fa61f32f59999912e6
Shailesh <shaileshp@gmail.com>
2021-12-23 09:27:42 +0000

    Added read subscription connector

---
+++ app/connectors/SubscriptionConnector.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.FrontendAppConfig
+import models.subscription.ResponseDetail
+import play.api.Logging
+import uk.gov.hmrc.http.HttpReads.Implicits.readRaw
+import uk.gov.hmrc.http.HttpReads.is2xx
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class SubscriptionConnector @Inject() (val config: FrontendAppConfig, val http: HttpClient) extends Logging {
+
+  def readSubscription()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Option[ResponseDetail]] = {
+
+    val url = s"${config.mdrUrl}/mandatory-disclosure-rules/subscription/read-subscription"
+    http
+      .POSTString(url, "")
+      .map {
+        case responseMessage if is2xx(responseMessage.status) =>
+          responseMessage.json
+            .asOpt[ResponseDetail]
+        case otherStatus =>
+          logger.warn(s"Status $otherStatus has been thrown when display subscription was called")
+          None
+      }
+      .recover {
+        case e: Exception =>
+          logger.warn(s"S${e.getMessage} has been thrown when display subscription was called")
+          None
+      }
+  }
+
+}
