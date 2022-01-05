654b3ec77c87e090eae8fd118e241065109b0ac1
DarrenCB <58850786+DarrenCB@users.noreply.github.com>
2022-01-05 15:00:19 +0000

    update content and accessibility

--- app/viewmodels/govuk/FieldsetFluency.scala
+++ app/viewmodels/govuk/FieldsetFluency.scala
@@ -57,7 +57,7 @@ trait FieldsetFluency {
 
   implicit class FluentLegend(legend: Legend) {
 
-    def asPageHeading(size: LegendSize = LegendSize.ExtraLarge): Legend =
+    def asPageHeading(size: LegendSize = LegendSize.Large): Legend =
       legend
         .copy(isPageHeading = true)
         .withCssClass(size.toString)
