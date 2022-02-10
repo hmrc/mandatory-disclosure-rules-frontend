2d6919af14140d6fb3600fe0333ff1bd3f820692
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-02-10 15:03:17 +0000

    DAC6-1429: Update test to check json reads and writes

--- app/models/FileDetails.scala
+++ app/models/FileDetails.scala
@@ -1,25 +1,52 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models
 
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.{Json, OFormat, Reads, Writes}
 
-  import java.time.LocalDateTime
+import java.time.LocalDateTime
 
 sealed trait FileStatus
 case object Pending extends FileStatus
-case class Rejected(error:FileError) extends FileStatus
+case class Rejected(error: FileError) extends FileStatus
 case object Accepted extends FileStatus
 
-case class FileError(detail:String)
+object Rejected {
+  implicit def format: OFormat[Rejected] = Json.format[Rejected]
+}
+
+object FileStatus {
+
+  implicit val format: OFormat[FileStatus] = {
+    implicit def accepted: OFormat[Accepted.type] = Json.format[Accepted.type]
+    implicit def pend: OFormat[Pending.type]      = Json.format[Pending.type]
+
+    Json.format[FileStatus]
+  }
+}
+
+case class FileError(detail: String)
 
 object FileError {
   implicit val format: OFormat[FileError] = Json.format[FileError]
 }
 
+case class FileDetails(name: String, submitted: LocalDateTime, status: FileStatus, conversationId: String)
 
-case class FileDetails(name:String, submitted:LocalDateTime, status: FileStatus, conversationId:String)
-
-object FileDetails{
+object FileDetails {
   implicit val format: OFormat[FileDetails] = Json.format[FileDetails]
 }
-
-
