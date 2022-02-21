6875f4794b4a9e628d04af3da5e17515741d80cd
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-02-21 10:20:43 +0000

    DAC6-1434: Fix contact-frontend details

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -29,7 +29,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val host: String    = configuration.get[String]("host")
   val appName: String = configuration.get[String]("appName")
 
-  private val contactHost                  = configuration.get[String]("contact-frontend.host")
+  private val contactHost                  = servicesConfig.baseUrl("contact-frontend")
   private val contactFormServiceIdentifier = "mandatory-disclosure-rules-frontend"
 
   def feedbackUrl(implicit request: RequestHeader): String =
