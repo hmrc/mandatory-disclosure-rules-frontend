09302775b9edb47f5cb0df0347666bebf5bc4b9e
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-03-21 11:59:19 +0000

    DAC6-1540: Update content on Upload file page

--- app/assets/javascripts/upload-spinner.js
+++ app/assets/javascripts/upload-spinner.js
@@ -22,7 +22,7 @@ if(fileLength === 0){
 
    function addSpinner(){
        $("#file-upload").after(
-       "<div id=\"processing\" aria-live=\"polite\" class=\"govuk-!-margin-bottom-5\">" +
+       "<div id=\"processing\" aria-live=\"polite\" class=\"govuk-!-margin-bottom-5 govuk-!-margin-top-5\">" +
        "<h2 class=\"govuk-heading-m\">"+ $("#processingMessage").val() +
        "</h2><div><div class=\"ccms-loader\"></div></div></div>"
        )
