9d5de71cde64b7173410f3c518db4e1399b54001
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-03-21 11:19:20 +0000

    DAC6-1540: Add new paragraph for Upload file page

--- app/assets/javascripts/upload-spinner.js
+++ app/assets/javascripts/upload-spinner.js
@@ -21,7 +21,7 @@ if(fileLength === 0){
     };
 
    function addSpinner(){
-       $("#file-upload").before(
+       $("#file-upload").after(
        "<div id=\"processing\" aria-live=\"polite\" class=\"govuk-!-margin-bottom-5\">" +
        "<h2 class=\"govuk-heading-m\">"+ $("#processingMessage").val() +
        "</h2><div><div class=\"ccms-loader\"></div></div></div>"
