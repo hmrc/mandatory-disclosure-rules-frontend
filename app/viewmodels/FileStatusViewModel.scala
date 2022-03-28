d44228848ebe29985993e27d1e5a10557f0fcec4
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2022-03-28 14:11:21 +0100

    DAC6-1546: Update 'file status' page style

--- app/viewmodels/FileStatusViewModel.scala
+++ app/viewmodels/FileStatusViewModel.scala
@@ -80,7 +80,12 @@ object FileStatusViewModel {
         HeadCell(Text(Messages("fileStatus.nextSteps")), classes = "app-custom-class")
       )
     )
-    Table(rows = tableRow, head = header,
-      firstCellIsHeader = true, caption = Some(Messages("fileStatus.heading")), captionClasses = "govuk-table__caption govuk-visually-hidden")
+    Table(
+      rows = tableRow,
+      head = header,
+      firstCellIsHeader = true,
+      caption = Some(Messages("fileStatus.heading")),
+      captionClasses = "govuk-table__caption govuk-visually-hidden"
+    )
   }
 }
