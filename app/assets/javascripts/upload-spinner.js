b42d4601679fcdf6447d0075f9154f3c87b9c6b3
Shailesh Patil <shaileshp@gmail.com>
2022-05-12 11:26:10 +0100

    Added time out (#123)
    * Added time out
    
    * Added time out
    
    * removed the time out

--- app/assets/javascripts/upload-spinner.js
+++ app/assets/javascripts/upload-spinner.js
@@ -30,13 +30,13 @@ if(fileLength === 0){
 
     function fileUpload(form){
         if (inProgress === false) {
-            $.ajax({
+                $.ajax({
                   url: form.action,
                   type: "POST",
                   data: new FormData(form),
                   processData: false,
                   contentType: false,
-                  crossDomain: true
+                  crossDomain   : true
             }).fail(function(jqXHR, textStatus, errorThrown ){
                 submitError("4000", jqXHR)
             }).done(function(){
@@ -72,3 +72,11 @@ function refreshPage(){
     }
 
 }
+
+$(document).ready(function ()
+{
+  var hasError = (window.location.href.indexOf("errorCode") > -1)
+  var preFixError = hasError ? "Error: " : ""
+  var appendError = preFixError + $("title").html()
+  $("title").html(appendError)
+});
