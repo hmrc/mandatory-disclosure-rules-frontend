633ae2e1083ebf9b2b39b1aa802c46de261121e6
Kolade Adewuyi <koladeadewuyi@users.noreply.github.com>
2023-06-28 09:28:58 +0100

    DAC6-2525: Fix platops-pr-bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapVersion = "7.16.0"
+  private val bootstrapVersion = "7.19.0"
   private val mongoVersion = "1.3.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.12.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.14.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % mongoVersion,
