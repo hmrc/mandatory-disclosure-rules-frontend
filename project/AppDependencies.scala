7f754236ea84af09e9473aa97265564ede631386
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2025-08-21 14:40:21 +0100

    Upgrade play frontend bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.18.0"
+  private val bootstrapVersion = "9.19.0"
   private val mongoVersion     = "2.7.0"
   private val playVersion      = "play-30"
 
