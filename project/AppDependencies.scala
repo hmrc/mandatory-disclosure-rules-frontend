cecbb54d724dd13ba0ab2efb95277d7ace793dce
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-03-13 14:24:32 +0000

    DDCYLS-8142: dependency upgrades to fix vulnerabilities (#264)
    * Amending header year
    
    * PR Bot Comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.4.0"
-  private val mongoVersion     = "2.10.0"
+  private val bootstrapVersion = "10.7.0"
+  private val mongoVersion     = "2.12.0"
   private val playVersion      = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.20.0",
-    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.3.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.32.0",
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.5.0",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % mongoVersion,
     "org.typelevel"     %% "cats-core"                                   % "2.13.0"
